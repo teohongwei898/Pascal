b1673b5a60df6f15b5b5a8ffdcff640956c318dc
Hong Wei <teohongwei898@gmail.com>
2018-06-17 16:03:49 +0800

    Create Greetings.pas

---
+++ Greetings.pas
@@ -0,0 +1,18 @@
+program Greetings;
+const
+message = ' Welcome to the world of Pascal ';
+
+type
+name = string;
+var
+firstname, surname: name;
+
+begin
+   writeln('Please enter your first name: ');
+   readln(firstname);
+   
+   writeln('Please enter your surname: ');
+   readln(surname);
+   
+   writeln;
+   write
